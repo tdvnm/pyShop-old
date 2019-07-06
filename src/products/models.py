fff9ed29e437a8b7defb0e5230102e0aa4a29eb2
shubhro <shubhrogupta117@gmail.com>
2019-07-06 14:14:25 +0530

    uhuh

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=25)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.ImageField

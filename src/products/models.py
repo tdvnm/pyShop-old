3d9243b2b548df7b469a3863d0ec3459b2327bb7
shubhro <shubhrogupta117@gmail.com>
2019-07-07 06:53:13 +0530

    mhmmmmmmmmmm

--- src/products/models.py
+++ src/products/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=25)
     price = models.FloatField()
     stock = models.IntegerField()
-    image_url = models.ImageField
+    image_url = models.CharField(max_length=2048)
